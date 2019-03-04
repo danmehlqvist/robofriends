dbfe7ca7055e61ebda243933dca1d893785375be
Dan Mehlqvist <dan.mehlqvist@gmail.com>
2019-03-04 21:14:00 +0100

    Updates

---
+++ precache-manifest.15e07d39c430eaff93e11464b481c3e7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "aedb4e53e89ac656e8a6",
+    "url": "/robofriends/static/js/runtime~main.aedb4e53.js"
+  },
+  {
+    "revision": "e710db904f27725a5394",
+    "url": "/robofriends/static/js/main.e710db90.chunk.js"
+  },
+  {
+    "revision": "3914a9a61015c8c17df0",
+    "url": "/robofriends/static/js/2.3914a9a6.chunk.js"
+  },
+  {
+    "revision": "e710db904f27725a5394",
+    "url": "/robofriends/static/css/main.65018cab.chunk.css"
+  },
+  {
+    "revision": "3914a9a61015c8c17df0",
+    "url": "/robofriends/static/css/2.4a1c4acd.chunk.css"
+  },
+  {
+    "revision": "ee73a8ef9939798fb2d953ad61175b1c",
+    "url": "/robofriends/index.html"
+  }
+];
